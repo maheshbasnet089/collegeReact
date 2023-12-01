af7e65a3b3be1d72668566f23eaa9162456af6f3
maheshbasnet089 <basnetmanish089@gmail.com>
2023-12-01 09:31:31 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Button from './Button'
+
+function App() {
+
+  return (
+    <div>
+      <Button buttonVitraKoText="Login" />
+      <Button buttonVitraKoText="Register" />
+      <Button buttonVitraKoText="Click More" />
+      <Button buttonVitraKoText="See More" />
+      <Button buttonVitraKoText="Register" />
+     
+    </div> 
+  )
+}
+
+
+export default App
